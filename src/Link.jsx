21f69e2919086052e68c710bfe5047c169eee581
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-07-25 10:52:20 +0200

    create Router component

--- src/Link.jsx
+++ src/Link.jsx
@@ -1,6 +1,6 @@
 import { EVENTS } from "./consts";
 
-export function navigate(href) {
+function navigate(href) {
   window.history.pushState({}, "", href);
   // create personalized event
   const navigationEvent = new Event(EVENTS.PUSHSTATE);
